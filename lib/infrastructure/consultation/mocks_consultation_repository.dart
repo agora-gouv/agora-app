1f524e0c0abf4c4696985c4997617ec26b18d546
Ziyu YE <ziyu.ye97@gmail.com>
2023-04-12 17:40:11 +0200

    feat-14: uncork display question of consultation

--- lib/infrastructure/consultation/mocks_consultation_repository.dart
+++ lib/infrastructure/consultation/mocks_consultation_repository.dart
@@ -1,8 +1,8 @@
 import 'package:agora/common/client/agora_http_client.dart';
+import 'package:agora/common/extension/string_extension.dart';
 import 'package:agora/domain/consultation/details/consultation_details.dart';
 import 'package:agora/domain/consultation/questions/consultation_question.dart';
 import 'package:agora/domain/consultation/questions/consultation_question_response_choice.dart';
-import 'package:agora/domain/consultation/questions/consultation_question_type.dart';
 import 'package:agora/domain/consultation/questions/responses/consultation_question_response.dart';
 import 'package:agora/domain/consultation/summary/consultation_summary.dart';
 import 'package:agora/domain/consultation/summary/consultation_summary_et_ensuite.dart';
@@ -45,78 +45,31 @@ class MockConsultationSuccessRepository extends ConsultationRepository {
   Future<GetConsultationQuestionsRepositoryResponse> fetchConsultationQuestions({
     required String consultationId,
   }) async {
-    return GetConsultationQuestionsSucceedResponse(
-      consultationQuestions: [
-        ConsultationQuestion(
-          id: "questionIdD",
-          label: "Donnez un feedback",
-          order: 3,
-          type: ConsultationQuestionType.ouverte,
-          responseChoices: [],
-        ),
-        ConsultationQuestion(
-          id: "questionIdE",
-          label: "Avez vous des conseilles à ajouter?",
-          order: 5,
-          type: ConsultationQuestionType.ouverte,
-          responseChoices: [],
-        ),
-        ConsultationQuestion(
-          id: "questionIdB",
-          label:
-              "Si vous vous lancez dans le co-voiturage, vous pouvez bénéficier d’une prime de 100 euros. Etes-vous intéressé(e) pour vous lancer ?",
-          order: 2,
-          type: ConsultationQuestionType.unique,
-          responseChoices: [
-            ConsultationQuestionResponseChoice(id: "choiceAA", label: "non", order: 2),
-            ConsultationQuestionResponseChoice(id: "choiceBB", label: "oui", order: 1),
-          ],
-        ),
-        ConsultationQuestion(
-          id: "questionIdA",
-          label: "Comment vous rendez-vous généralement sur votre lieu de travail ?",
-          order: 1,
-          type: ConsultationQuestionType.unique,
-          responseChoices: [
-            ConsultationQuestionResponseChoice(id: "choiceA", label: "En vélo ou à pied", order: 4),
-            ConsultationQuestionResponseChoice(id: "choiceB", label: "En co-voiturage", order: 2),
-            ConsultationQuestionResponseChoice(id: "choiceC", label: "En transports en commun", order: 3),
-            ConsultationQuestionResponseChoice(id: "choiceD", label: "Je ne suis pas concerné", order: 5),
-            ConsultationQuestionResponseChoice(id: "choiceE", label: "En voiture, seul(e)", order: 1),
-          ],
-        ),
-        ConsultationQuestion(
-          id: "questionIdC",
-          label: "Pour quelle raison principale ?",
-          order: 4,
-          type: ConsultationQuestionType.unique,
-          responseChoices: [
-            ConsultationQuestionResponseChoice(
-              id: "choiceAAA",
-              label: "Je veux être tranquille dans ma voiture",
-              order: 4,
+    try {
+      final response = await httpClient.get("/consultations/$consultationId/questions");
+      final questions = (response.data["questions"] as List)
+          .map(
+            (question) => ConsultationQuestion(
+              id: question["id"] as String,
+              label: question["label"] as String,
+              order: question["order"] as int,
+              type: (question["type"] as String).toConsultationQuestionType(),
+              responseChoices: (question["possibleChoices"] as List)
+                  .map(
+                    (responseChoice) => ConsultationQuestionResponseChoice(
+                      id: responseChoice["id"] as String,
+                      label: responseChoice["label"] as String,
+                      order: responseChoice["order"] as int,
+                    ),
+                  )
+                  .toList(),
             ),
-            ConsultationQuestionResponseChoice(
-              id: "choiceBBB",
-              label: "Je fais des trajets différents chaque jour",
-              order: 2,
-            ),
-            ConsultationQuestionResponseChoice(
-              id: "choiceCCC",
-              label: "Je crains de ne pas être à l’heure en co-voiturant",
-              order: 3,
-            ),
-            ConsultationQuestionResponseChoice(id: "choiceDDD", label: "Je crains pour ma sécurité", order: 5),
-            ConsultationQuestionResponseChoice(
-              id: "choiceEEE",
-              label: "Je veux être flexible en cas d’imprévu",
-              order: 1,
-            ),
-            ConsultationQuestionResponseChoice(id: "choiceFFF", label: "Autre", order: 6),
-          ],
-        ),
-      ],
-    );
+          )
+          .toList();
+      return GetConsultationQuestionsSucceedResponse(consultationQuestions: questions);
+    } catch (e) {
+      return GetConsultationQuestionsFailedResponse();
+    }
   }
 
   @override
