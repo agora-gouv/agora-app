edf1d72f0c08f9de7df6ef5b31678e525cf6351e
Ziyu YE <ziyu.ye97@gmail.com>
2023-04-12 10:37:22 +0200

    feat-28: add test screen

--- lib/pages/loading_page.dart
+++ lib/pages/loading_page.dart
@@ -1,9 +1,14 @@
 import 'package:agora/bloc/thematique/thematique_bloc.dart';
 import 'package:agora/bloc/thematique/thematique_event.dart';
 import 'package:agora/bloc/thematique/thematique_state.dart';
+import 'package:agora/bloc/thematique/thematique_view_model.dart';
 import 'package:agora/common/client/repository_manager.dart';
+import 'package:agora/design/agora_button.dart';
+import 'package:agora/design/agora_button_style.dart';
+import 'package:agora/design/agora_spacings.dart';
 import 'package:agora/design/custom_view/agora_error_view.dart';
 import 'package:agora/design/custom_view/agora_scaffold.dart';
+import 'package:agora/design/custom_view/agora_thematique_card.dart';
 import 'package:agora/pages/consultation/consultation_details_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -18,21 +23,35 @@ class LoadingPage extends StatelessWidget {
         repository: RepositoryManager.getThematiqueRepository(),
       )..add(FetchThematiqueEvent()),
       child: AgoraScaffold(
-        child: BlocConsumer<ThematiqueBloc, ThematiqueState>(
-          listener: (context, state) {
-            if (state is ThematiqueSuccessState) {
-              Navigator.pushNamed(
-                context,
-                ConsultationDetailsPage.routeName,
-                arguments: BlocProvider.of<ThematiqueBloc>(context),
-              );
-            }
-          },
+        child: BlocBuilder<ThematiqueBloc, ThematiqueState>(
           builder: (context, state) {
             if (state is ThematiqueInitialLoadingState) {
               return Center(child: CircularProgressIndicator());
             } else if (state is ThematiqueErrorState) {
               return Center(child: AgoraErrorView());
+            } else if (state is ThematiqueSuccessState) {
+              return SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: _buildThematiques(state.viewModel) +
+                      [
+                        SizedBox(height: AgoraSpacings.x2),
+                        AgoraButton(
+                          label: "Détails d'une consultation",
+                          style: AgoraButtonStyle.primaryButtonStyle,
+                          onPressed: () {
+                            Navigator.pushNamed(
+                              context,
+                              ConsultationDetailsPage.routeName,
+                              arguments: BlocProvider.of<ThematiqueBloc>(context),
+                            );
+                          },
+                        ),
+                        SizedBox(height: AgoraSpacings.x3),
+                      ],
+                ),
+              );
             } else {
               return Container();
             }
@@ -40,5 +59,20 @@ class LoadingPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  List<Widget> _buildThematiques(List<ThematiqueViewModel> thematiqueViewModels) {
+    final List<Widget> widgets = [];
+    for (var thematiqueViewModel in thematiqueViewModels) {
+      widgets.add(
+        AgoraThematiqueCard(
+          picto: thematiqueViewModel.picto,
+          label: thematiqueViewModel.label,
+          color: thematiqueViewModel.color,
+        ),
+      );
+      widgets.add(SizedBox(height: AgoraSpacings.base));
+    }
+    return widgets;
   }
 }
