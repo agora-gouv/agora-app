cc3c4abf2cd4a01113b687bf7a1c8b990e5b5a95
Ziyu YE <ziyu.ye97@gmail.com>
2023-04-14 10:36:02 +0200

    feat-22: uncork consultation question's responses

--- lib/infrastructure/consultation/mocks_consultation_repository.dart
+++ lib/infrastructure/consultation/mocks_consultation_repository.dart
@@ -1,45 +1,12 @@
-import 'package:agora/common/client/agora_http_client.dart';
-import 'package:agora/domain/consultation/details/consultation_details.dart';
 import 'package:agora/domain/consultation/questions/consultation_question.dart';
 import 'package:agora/domain/consultation/questions/consultation_question_response_choice.dart';
 import 'package:agora/domain/consultation/questions/consultation_question_type.dart';
 import 'package:agora/domain/consultation/questions/responses/consultation_question_response.dart';
-import 'package:agora/domain/consultation/summary/consultation_summary.dart';
-import 'package:agora/domain/consultation/summary/consultation_summary_et_ensuite.dart';
-import 'package:agora/domain/consultation/summary/consultation_summary_results.dart';
 import 'package:agora/infrastructure/consultation/consultation_repository.dart';
 
 // TODO suppress when debouncing is done
-class MockConsultationSuccessRepository extends ConsultationRepository {
-  final AgoraDioHttpClient httpClient;
-
-  MockConsultationSuccessRepository({required this.httpClient});
-
-  @override
-  Future<GetConsultationDetailsRepositoryResponse> fetchConsultationDetails({
-    required String consultationId,
-  }) async {
-    try {
-      final response = await httpClient.get("/consultations/$consultationId");
-      return GetConsultationDetailsSucceedResponse(
-        consultationDetails: ConsultationDetails(
-          id: response.data["id"] as String,
-          title: response.data["title"] as String,
-          cover: response.data["coverUrl"] as String,
-          thematiqueId: response.data["thematiqueId"] as String,
-          endDate: DateTime.parse(response.data["endDate"] as String),
-          questionCount: response.data["questionCount"] as String,
-          estimatedTime: response.data["estimatedTime"] as String,
-          participantCount: response.data["participantCount"] as int,
-          participantCountGoal: response.data["participantCountGoal"] as int,
-          description: response.data["description"] as String,
-          tipsDescription: response.data["tipsDescription"] as String,
-        ),
-      );
-    } catch (e) {
-      return GetConsultationDetailsFailedResponse();
-    }
-  }
+class MockConsultationSuccessRepository extends ConsultationDioRepository {
+  MockConsultationSuccessRepository({required super.httpClient});
 
   @override
   Future<GetConsultationQuestionsRepositoryResponse> fetchConsultationQuestions({
@@ -130,52 +97,5 @@ class MockConsultationSuccessRepository extends ConsultationRepository {
     required List<ConsultationQuestionResponses> questionsResponses,
   }) async {
     return SendConsultationResponsesSucceedResponse();
-  }
-
-  @override
-  Future<GetConsultationSummaryRepositoryResponse> fetchConsultationSummary({
-    required String consultationId,
-  }) async {
-    return GetConsultationSummarySucceedResponse(
-      consultationSummary: ConsultationSummary(
-        title: "Développer le covoiturage au quotidien",
-        participantCount: 15035,
-        results: [
-          ConsultationSummaryResults(
-            questionTitle: "Les déplacements professionnels en covoiturage",
-            responses: [
-              ConsultationSummaryResponse(label: "En voiture seul", ratio: 65),
-              ConsultationSummaryResponse(label: "En transports en commun, vélo, à pied", ratio: 17),
-              ConsultationSummaryResponse(label: "Je ne suis pas concerné", ratio: 16),
-              ConsultationSummaryResponse(label: "En co-voiturage", ratio: 2),
-            ],
-          ),
-          ConsultationSummaryResults(
-            questionTitle:
-                "Si vous vous lancez dans le co-voiturage, vous pouvez bénéficier d’une prime de 100 euros. Etes-vous intéressé(e) pour vous lancer ?",
-            responses: [
-              ConsultationSummaryResponse(label: "Oui", ratio: 65),
-              ConsultationSummaryResponse(label: "Non", ratio: 35),
-            ],
-          ),
-          ConsultationSummaryResults(
-            questionTitle: "Pour quelle raison principale ?",
-            responses: [
-              ConsultationSummaryResponse(label: "Je veux être tranquille dans ma voiture", ratio: 42),
-              ConsultationSummaryResponse(label: "Je veux être flexible en cas d’imprévu", ratio: 25),
-              ConsultationSummaryResponse(label: "Je crains pour ma sécurité", ratio: 16),
-              ConsultationSummaryResponse(label: "Je crains de ne pas être à l’heure en co-voiturant", ratio: 8),
-              ConsultationSummaryResponse(label: "Je fais des trajets différents chaque jour", ratio: 7),
-              ConsultationSummaryResponse(label: "Autre", ratio: 2),
-            ],
-          ),
-        ],
-        etEnsuite: ConsultationSummaryEtEnsuite(
-          step: 1,
-          description:
-              "<body>La description avec textes <b>en gras</b> et potentiellement des <a href=\"https://google.fr\">liens</a><br/><br/><ul><li>example1 <b>en gras</b></li><li>example2</li></ul></body>",
-        ),
-      ),
-    );
   }
 }
