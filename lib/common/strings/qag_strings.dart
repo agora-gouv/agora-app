a296c230b1df10df6588abac78534740403b5c1b
Ziyu YE <ziyu.ye97@gmail.com>
2023-04-19 13:45:29 +0200

    feat-51: ask question by email

--- lib/common/strings/qag_strings.dart
+++ lib/common/strings/qag_strings.dart
@@ -2,4 +2,24 @@ class QagStrings {
   static const String by = "Par";
   static const String at = "le";
   static const String share = "Partager";
+  static const String askQuestionTitle = "Poser une question au gouvernement";
+  static const String askQuestionDescription =
+      "Toutes les questions sont les bienvenues, du moment qu'elles se conforment à la charte de participation : porter sur un sujet d'intérêt général et avoir un ton respectueux.";
+  static const String readNotice = "Lire la charte";
+  static const String noticeTitle = "Charte de participation";
+  static const String noticeDescription = "lorem ipsum";
+  static const String agree = "C'est compris";
+  static const String questionTitle = "Question";
+  static const String questionHint = "Votre question";
+  static const String detailsTitle = "Détails supplémentaires";
+  static const String detailsHint = "Ajoutez davantage d’informations ou de contexte si vous le souhaitez.\n";
+  static const String thematiqueTitle = "Thématique";
+  static const String thematiqueDescription = "Choisissez votre thématique";
+  static const String yourNameTitle = "Votre prénom";
+  static const String yourNameHint = "David";
+  static const String askQuestionInformation =
+      "Durant la phase d’expérimentation, nous vous proposons d’envoyer votre question par email. Elle apparaîtra dans les 48h sur l’application.";
+  static const String askQuestionConfigureMail = "Veuillez configurer l'application Mail par défaut de votre appareil.";
+  static const String askQuestionMailSubject = "Ma question au gouvernement";
+  static const String sendByEmail = "Envoyer par e-mail";
 }
