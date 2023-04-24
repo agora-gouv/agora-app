ef0179fc9a630ae5c0ce3e74db0843cf02b67d83
Ziyu YE <ziyu.ye97@gmail.com>
2023-04-24 09:45:19 +0200

    feat-58: uncork support qag

--- lib/infrastructure/qag/mocks_qag_repository.dart
+++ lib/infrastructure/qag/mocks_qag_repository.dart
@@ -3,14 +3,4 @@ import 'package:agora/infrastructure/qag/qag_repository.dart';
 // TODO suppress when debouncing is done
 class MockQagSuccessRepository extends QagDioRepository {
   MockQagSuccessRepository({required super.httpClient});
-
-  @override
-  Future<SupportQagRepositoryResponse> supportQag({required String qagId, required String deviceId}) async {
-    return SupportQagSucceedResponse();
-  }
-
-  @override
-  Future<DeleteSupportQagRepositoryResponse> deleteSupportQag({required String qagId, required String deviceId}) async {
-    return DeleteSupportQagSucceedResponse();
-  }
 }
