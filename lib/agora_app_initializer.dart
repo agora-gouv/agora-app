89139cfe3f2da14f6dc4198c9de5951e0aafeac3
Ziyu YE <ziyu.ye97@gmail.com>
2023-04-27 11:17:59 +0200

    feat-63: configure notification

--- lib/agora_app_initializer.dart
+++ lib/agora_app_initializer.dart
@@ -1,12 +1,26 @@
 import 'package:agora/agora_app.dart';
+import 'package:agora/common/client/service_manager.dart';
+import 'package:agora/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 
 class AgoraInitializer {
   static void initializeApp() async {
+    WidgetsFlutterBinding.ensureInitialized();
     Intl.defaultLocale = "fr_FR";
     initializeDateFormatting('fr_FR', null);
+
+    await _setupNotification();
     runApp(AgoraApp());
+  }
+
+  static Future<void> _setupNotification() async {
+    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+    if (!kIsWeb) {
+      await ServiceManager.getPushNotificationService().setupNotifications();
+    }
   }
 }
