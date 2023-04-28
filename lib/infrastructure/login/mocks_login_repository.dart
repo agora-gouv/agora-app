5b397bb221e3736ea3d4af9e1c6deffab0d782d1
Ziyu YE <ziyu.ye97@gmail.com>
2023-04-28 13:49:18 +0200

    feat-63: uncork send fcm token

--- lib/infrastructure/login/mocks_login_repository.dart
+++ lib/infrastructure/login/mocks_login_repository.dart
@@ -1,16 +1,6 @@
-import 'package:agora/common/manager/helper_manager.dart';
 import 'package:agora/infrastructure/login/login_repository.dart';
 
 // TODO suppress when debouncing is done
 class MockLoginSuccessRepository extends LoginDioRepository {
   MockLoginSuccessRepository({required super.httpClient});
-
-  @override
-  Future<LoginRepositoryResponse> login({
-    required String deviceId,
-    required String firebaseMessagingToken,
-  }) async {
-    final deviceId = await HelperManager.getDeviceInfoHelper().getDeviceId();
-    return LoginSucceedResponse(userId: deviceId!);
-  }
 }
