b0eb5b10eeb1812af4b9acec967dae0d58c6dd73
Ziyu YE <ziyu.ye97@gmail.com>
2023-05-04 09:01:24 +0200

    feat-66: uncork ongoing consultation

--- lib/infrastructure/consultation/repository/mocks_consultation_repository.dart
+++ lib/infrastructure/consultation/repository/mocks_consultation_repository.dart
@@ -1,33 +1,5 @@
-import 'package:agora/domain/consultation/consultation.dart';
-import 'package:agora/domain/thematique/thematique.dart';
 import 'package:agora/infrastructure/consultation/repository/consultation_repository.dart';
 
 class MockConsultationSuccessRepository extends ConsultationDioRepository {
   MockConsultationSuccessRepository({required super.httpClient});
-
-  @override
-  Future<GetConsultationsRepositoryResponse> fetchConsultations({
-    required String deviceId,
-  }) async {
-    return GetConsultationsSucceedResponse(
-      consultations: [
-        ConsultationOngoing(
-          id: "c29255f2-10ca-4be5-aab1-801ea173337c",
-          title: "Développer le covoiturage au quotidien",
-          coverUrl: "https://betagouv.github.io/agora-content/covoiturage.png",
-          thematique: Thematique(picto: "🚊", label: "Transports", color: "#FFFCF7CF"),
-          endDate: DateTime(2024, 1, 23),
-          hasAnswered: false,
-        ),
-        ConsultationOngoing(
-          id: "c29255f2-10ca-4be5-aab1-801ea173337c",
-          title: "Développer le covoiturage au quotidien",
-          coverUrl: "https://betagouv.github.io/agora-content/covoiturage.png",
-          thematique: Thematique(picto: "🚊", label: "Transports", color: "#FFFCF7CF"),
-          endDate: DateTime(2024, 1, 23),
-          hasAnswered: false,
-        ),
-      ],
-    );
-  }
 }
