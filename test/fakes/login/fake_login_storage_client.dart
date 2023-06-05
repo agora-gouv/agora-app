37a96ede46c63ad7e100cef40e97ac4b3200339d
Ziyu YE <ziyu.ye97@gmail.com>
2023-06-05 14:54:38 +0200

    feat-145: update signup and login

--- test/fakes/login/fake_login_storage_client.dart
+++ test/fakes/login/fake_login_storage_client.dart
@@ -3,13 +3,20 @@ import 'package:agora/infrastructure/login/login_storage_client.dart';
 import '../storage/fake_secure_storage_client.dart';
 
 class FakeLoginStorageClient extends LoginStorageClient {
+  String? userId;
   String? loginToken;
 
   FakeLoginStorageClient() : super(secureStorageClient: FakeSecureStorageClient());
 
   @override
-  void save(String loginToken) async {
+  void save({required String userId, required String loginToken}) async {
+    this.userId = userId;
     this.loginToken = loginToken;
+  }
+
+  @override
+  Future<String?> getUserId() async {
+    return userId;
   }
 
   @override
