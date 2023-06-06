02f91b21e68dc54e9d6f2e33a69d0991be119ec9
Ziyu YE <ziyu.ye97@gmail.com>
2023-06-06 17:24:34 +0200

    feat-160: add pop up to question 6

--- lib/pages/demographic/question_view/demographic_vote_view.dart
+++ lib/pages/demographic/question_view/demographic_vote_view.dart
@@ -1,7 +1,10 @@
 import 'package:agora/common/strings/demographic_strings.dart';
+import 'package:agora/common/strings/generic_strings.dart';
+import 'package:agora/design/custom_view/agora_alert_dialog.dart';
 import 'package:agora/design/custom_view/agora_demographic_simple_view.dart';
 import 'package:agora/design/custom_view/button/agora_button.dart';
 import 'package:agora/design/style/agora_button_style.dart';
+import 'package:agora/design/style/agora_colors.dart';
 import 'package:agora/design/style/agora_spacings.dart';
 import 'package:agora/design/style/agora_text_styles.dart';
 import 'package:agora/domain/demographic/demographic_question_type.dart';
@@ -39,6 +42,27 @@ class _DemographicVoteViewState extends State<DemographicVoteView> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        InkWell(
+          onTap: () {
+            showAgoraDialog(
+              context: context,
+              columnChildren: [
+                Text(DemographicStrings.whyAskContent, style: AgoraTextStyles.light16),
+                SizedBox(height: AgoraSpacings.x0_75),
+                AgoraButton(
+                  label: GenericStrings.close,
+                  style: AgoraButtonStyle.primaryButtonStyle,
+                  onPressed: () => Navigator.pop(context),
+                ),
+              ],
+            );
+          },
+          child: Text(
+            DemographicStrings.whyAsk,
+            style: AgoraTextStyles.regular14Underline.copyWith(color: AgoraColors.primaryBlue),
+          ),
+        ),
+        SizedBox(height: AgoraSpacings.base),
         Text(DemographicStrings.question6_1, style: AgoraTextStyles.medium18),
         SizedBox(height: AgoraSpacings.x0_75),
         _buildResponseRow(
