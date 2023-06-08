1bd297fd51f56c510898a4656befdf2f54336713
Ziyu YE <ziyu.ye97@gmail.com>
2023-06-08 17:51:40 +0200

    feedback: add copy button

--- lib/pages/profile/delete_account_page.dart
+++ lib/pages/profile/delete_account_page.dart
@@ -4,12 +4,16 @@ import 'package:agora/common/strings/demographic_strings.dart';
 import 'package:agora/common/strings/generic_strings.dart';
 import 'package:agora/common/strings/profile_strings.dart';
 import 'package:agora/design/custom_view/agora_rich_text.dart';
+import 'package:agora/design/custom_view/agora_rounded_card.dart';
 import 'package:agora/design/custom_view/agora_scaffold.dart';
 import 'package:agora/design/custom_view/agora_secondary_style_view.dart';
 import 'package:agora/design/custom_view/agora_single_scroll_view.dart';
+import 'package:agora/design/style/agora_colors.dart';
+import 'package:agora/design/style/agora_corners.dart';
 import 'package:agora/design/style/agora_spacings.dart';
 import 'package:agora/design/style/agora_text_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class DeleteAccountPage extends StatelessWidget {
   static const routeName = "/deleteAccountPage";
@@ -39,6 +43,7 @@ class DeleteAccountPage extends StatelessWidget {
               if (!snapshot.hasData) {
                 return Center(child: CircularProgressIndicator());
               } else {
+                final userId = snapshot.data;
                 return Padding(
                   padding: const EdgeInsets.symmetric(horizontal: AgoraSpacings.horizontalPadding),
                   child: Column(
@@ -54,14 +59,32 @@ class DeleteAccountPage extends StatelessWidget {
                               child: Column(
                                 children: [
                                   SizedBox(height: AgoraSpacings.base),
-                                  GestureDetector(
-                                    onLongPress: () => ClipboardHelper.copy(context, GenericStrings.mailSupport),
-                                    child: Center(
-                                      child: Text(
-                                        GenericStrings.mailSupport,
-                                        style: AgoraTextStyles.medium16,
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      SizedBox(width: 40),
+                                      SizedBox(width: AgoraSpacings.x0_75),
+                                      Expanded(
+                                        child: GestureDetector(
+                                          onLongPress: () => _copySupportEmail(context),
+                                          child: Center(
+                                            child: Text(
+                                              GenericStrings.mailSupport,
+                                              style: AgoraTextStyles.medium16,
+                                              textAlign: TextAlign.center,
+                                            ),
+                                          ),
+                                        ),
                                       ),
-                                    ),
+                                      SizedBox(width: AgoraSpacings.x0_75),
+                                      AgoraRoundedCard(
+                                        cardColor: AgoraColors.transparent,
+                                        cornerRadius: AgoraCorners.rounded50,
+                                        onTap: () => _copySupportEmail(context),
+                                        padding: const EdgeInsets.all(AgoraSpacings.x0_5),
+                                        child: SvgPicture.asset("assets/ic_copy.svg"),
+                                      ),
+                                    ],
                                   ),
                                   SizedBox(height: AgoraSpacings.base),
                                 ],
@@ -75,19 +98,31 @@ class DeleteAccountPage extends StatelessWidget {
                               child: Column(
                                 children: [
                                   SizedBox(height: AgoraSpacings.base),
-                                  GestureDetector(
-                                    onLongPress: () {
-                                      final userId = snapshot.data;
-                                      if (userId != null) {
-                                        ClipboardHelper.copy(context, userId);
-                                      }
-                                    },
-                                    child: Center(
-                                      child: Text(
-                                        snapshot.data ?? "",
-                                        style: AgoraTextStyles.medium16,
+                                  Row(
+                                    children: [
+                                      SizedBox(width: 40),
+                                      SizedBox(width: AgoraSpacings.x0_75),
+                                      Expanded(
+                                        child: GestureDetector(
+                                          onLongPress: () => _copyUserId(userId, context),
+                                          child: Center(
+                                            child: Text(
+                                              snapshot.data ?? "",
+                                              style: AgoraTextStyles.medium16,
+                                              textAlign: TextAlign.center,
+                                            ),
+                                          ),
+                                        ),
                                       ),
-                                    ),
+                                      SizedBox(width: AgoraSpacings.x0_75),
+                                      AgoraRoundedCard(
+                                        cardColor: AgoraColors.transparent,
+                                        cornerRadius: AgoraCorners.rounded50,
+                                        onTap: () => _copyUserId(userId, context),
+                                        padding: const EdgeInsets.all(AgoraSpacings.x0_5),
+                                        child: SvgPicture.asset("assets/ic_copy.svg"),
+                                      )
+                                    ],
                                   ),
                                   SizedBox(height: AgoraSpacings.base),
                                 ],
@@ -105,5 +140,15 @@ class DeleteAccountPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _copySupportEmail(BuildContext context) {
+    ClipboardHelper.copy(context, GenericStrings.mailSupport);
+  }
+
+  void _copyUserId(String? userId, BuildContext context) {
+    if (userId != null) {
+      ClipboardHelper.copy(context, userId);
+    }
   }
 }
