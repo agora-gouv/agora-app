1cb3f98e69b4052bd927125820dbdd62f1a6a001
Ziyu YE <ziyu.ye97@gmail.com>
2023-06-09 14:56:18 +0200

    feat-168: update participate wording and icon

--- lib/common/strings/consultation_strings.dart
+++ lib/common/strings/consultation_strings.dart
@@ -9,7 +9,7 @@ class ConsultationStrings {
   static const String answeredConsultationPart2 = "consultations";
   static const String consultationEmpty = "La liste est vide.";
   static const String participate = "Participer";
-  static const String seeResults = "Voir résultats";
+  static const String seeResults = "Voir les résultats";
   static const String beginButton = "Commencer";
   static const String endDate = "Jusqu'au %s";
   static const String participantCount = "%s participants";
