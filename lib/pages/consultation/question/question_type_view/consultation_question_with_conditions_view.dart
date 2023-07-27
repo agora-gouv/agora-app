a92fb8cbc52357f90ea84fafed4c034360e4a733
Ziyu YE <ziyu.ye97@gmail.com>
2023-07-27 14:08:03 +0200

    feat-256: question with other choice (unique with condition)

--- lib/pages/consultation/question/question_type_view/consultation_question_with_conditions_view.dart
+++ lib/pages/consultation/question/question_type_view/consultation_question_with_conditions_view.dart
@@ -1,42 +1,80 @@
 import 'package:agora/bloc/consultation/question/consultation_questions_view_model.dart';
+import 'package:agora/common/extension/string_extension.dart';
+import 'package:agora/common/uuid/uuid_utils.dart';
 import 'package:agora/design/custom_view/agora_question_response_choice_view.dart';
+import 'package:agora/design/custom_view/button/agora_button.dart';
+import 'package:agora/design/style/agora_button_style.dart';
 import 'package:agora/design/style/agora_spacings.dart';
 import 'package:agora/domain/consultation/questions/responses/consultation_question_response.dart';
 import 'package:agora/pages/consultation/question/consultation_question_helper.dart';
 import 'package:agora/pages/consultation/question/question_type_view/consultation_question_view.dart';
 import 'package:flutter/material.dart';
 
-class ConsultationQuestionWithConditionsView extends StatelessWidget {
+class ConsultationQuestionWithConditionsView extends StatefulWidget {
   final ConsultationQuestionWithConditionViewModel questionWithConditions;
-  final ConsultationQuestionResponses? previousResponses;
+  final ConsultationQuestionResponses? previousSelectedResponses;
   final int totalQuestions;
-  final Function(String questionId, String responseId, String nextQuestionId) onWithConditionResponseTap;
+  final Function(
+    String questionId,
+    String responseId,
+    String otherResponse,
+    String nextQuestionId,
+  ) onWithConditionResponseTap;
   final VoidCallback onBackTap;
 
   ConsultationQuestionWithConditionsView({
     Key? key,
     required this.questionWithConditions,
-    required this.previousResponses,
+    required this.previousSelectedResponses,
     required this.totalQuestions,
     required this.onWithConditionResponseTap,
     required this.onBackTap,
   }) : super(key: key);
 
   @override
+  State<ConsultationQuestionWithConditionsView> createState() => _ConsultationQuestionWithConditionsViewState();
+}
+
+class _ConsultationQuestionWithConditionsViewState extends State<ConsultationQuestionWithConditionsView> {
+  String currentQuestionId = "";
+  String currentResponseId = "";
+  String otherResponseText = "";
+  String currentNextQuestionId = "";
+  bool showNextButton = false;
+  bool shouldResetPreviousResponses = true;
+
+  @override
   Widget build(BuildContext context) {
+    _resetPreviousResponses();
     return ConsultationQuestionView(
-      order: questionWithConditions.order,
-      totalQuestions: totalQuestions,
-      questionProgress: questionWithConditions.questionProgress,
-      title: questionWithConditions.title,
-      popupDescription: questionWithConditions.popupDescription,
+      order: widget.questionWithConditions.order,
+      totalQuestions: widget.totalQuestions,
+      questionProgress: widget.questionWithConditions.questionProgress,
+      title: widget.questionWithConditions.title,
+      popupDescription: widget.questionWithConditions.popupDescription,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           ..._buildWithConditionsResponse(),
+          if (showNextButton)
+            AgoraButton(
+              label: ConsultationQuestionHelper.buildNextButtonLabel(
+                order: widget.questionWithConditions.order,
+                totalQuestions: widget.totalQuestions,
+              ),
+              style: AgoraButtonStyle.primaryButtonStyle,
+              onPressed: currentResponseId.isNotBlank() && otherResponseText.isNotBlank()
+                  ? () => widget.onWithConditionResponseTap(
+                        widget.questionWithConditions.id,
+                        currentResponseId,
+                        otherResponseText,
+                        currentNextQuestionId,
+                      )
+                  : null,
+            ),
           ...ConsultationQuestionHelper.buildBackButton(
-            order: questionWithConditions.order,
-            onBackTap: onBackTap,
+            order: widget.questionWithConditions.order,
+            onBackTap: widget.onBackTap,
           ),
         ],
       ),
@@ -45,19 +83,34 @@ class ConsultationQuestionWithConditionsView extends StatelessWidget {
 
   List<Widget> _buildWithConditionsResponse() {
     final List<Widget> responseWidgets = [];
-    for (final response in questionWithConditions.responseChoicesViewModels) {
+    for (final response in widget.questionWithConditions.responseChoicesViewModels) {
       responseWidgets.add(
         AgoraQuestionResponseChoiceView(
           responseId: response.id,
           responseLabel: response.label,
           hasOpenTextField: response.hasOpenTextField,
           isSelected: _isResponseAlreadySelected(response.id),
-          previousOtherResponse: "",
+          previousOtherResponse: otherResponseText,
           onTap: (responseId) {
-            onWithConditionResponseTap(questionWithConditions.id, responseId, response.nextQuestionId);
+            currentResponseId = responseId;
+            currentNextQuestionId = response.nextQuestionId;
+            if (response.hasOpenTextField) {
+              setState(() => showNextButton = true);
+            } else {
+              widget.onWithConditionResponseTap(
+                widget.questionWithConditions.id,
+                responseId,
+                otherResponseText,
+                currentNextQuestionId,
+              );
+            }
           },
           onOtherResponseChanged: (responseId, otherResponse) {
-            // TODO
+            if (currentResponseId == responseId) {
+              setState(() => otherResponseText = otherResponse);
+            } else {
+              setState(() => otherResponseText = "");
+            }
           },
         ),
       );
@@ -66,7 +119,34 @@ class ConsultationQuestionWithConditionsView extends StatelessWidget {
     return responseWidgets;
   }
 
+  void _resetPreviousResponses() {
+    if (currentQuestionId != widget.questionWithConditions.id) {
+      currentQuestionId = widget.questionWithConditions.id;
+      shouldResetPreviousResponses = true;
+    }
+    if (shouldResetPreviousResponses) {
+      currentResponseId = "";
+      otherResponseText = "";
+      currentNextQuestionId = "";
+      final previousSelectedResponses = widget.previousSelectedResponses;
+      if (previousSelectedResponses != null) {
+        final previousResponseIds = previousSelectedResponses.responseIds;
+        if (!previousResponseIds.contains(UuidUtils.uuidZero) && previousResponseIds.isNotEmpty) {
+          currentResponseId = previousResponseIds[0];
+          otherResponseText = previousSelectedResponses.responseText;
+          currentNextQuestionId = widget.questionWithConditions.responseChoicesViewModels
+              .firstWhere((element) => element.id == currentResponseId)
+              .nextQuestionId;
+          if (otherResponseText.isNotBlank()) {
+            showNextButton = true;
+          }
+        }
+      }
+      shouldResetPreviousResponses = false;
+    }
+  }
+
   bool _isResponseAlreadySelected(String responseId) {
-    return previousResponses != null ? previousResponses!.responseIds.contains(responseId) : false;
+    return currentResponseId == responseId;
   }
 }
