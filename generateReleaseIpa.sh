8882fd460931f89386d2f9cd307d7bc20072102e
Thierry LEE <thierry.leet@gmail.com>
2023-09-26 18:23:15 +0200

    Version code updated to 16

--- generateReleaseIpa.sh
+++ generateReleaseIpa.sh
@@ -1,3 +1,3 @@
 #flutter build ipa --build-name="$VERSION_NAME" --build-number=$VERSION_CODE --export-options-plist=ios/Params/ExportOptionsAdHoc.plist -t lib/$ENTRYPOINT --flavor=$FLAVOR
-#flutter build ipa --target=lib/main_dev.dart --flavor=dev --dart-define=app.flavor=Dev --export-options-plist=ios/Params/ExportOptionsAdHoc.plist --build-name="1.0" --build-number=18
-flutter build ipa --target=lib/main_prod.dart --flavor=prod --dart-define=app.flavor=Prod --export-options-plist=ios/Params/ExportOptionsProd.plist --build-name="1.0" --build-number=13
+#flutter build ipa --target=lib/main_dev.dart --flavor=dev --dart-define=app.flavor=Dev --export-options-plist=ios/Params/ExportOptionsAdHoc.plist --build-name="1.0.1" --build-number=22
+flutter build ipa --target=lib/main_prod.dart --flavor=prod --dart-define=app.flavor=Prod --export-options-plist=ios/Params/ExportOptionsProd.plist --build-name="1.0.1" --build-number=16
