9fce46bb323c7e93ff3c07b07713abe368a9a1e0
Weslay <weslay.mede@orange.fr>
2023-11-14 19:48:13 +0100

    wip

--- lib/pages/qag/details/qag_details_page.dart
+++ lib/pages/qag/details/qag_details_page.dart
@@ -15,6 +15,7 @@ import 'package:agora/common/manager/repository_manager.dart';
 import 'package:agora/common/strings/generic_strings.dart';
 import 'package:agora/common/strings/qag_strings.dart';
 import 'package:agora/design/custom_view/agora_error_view.dart';
+import 'package:agora/design/custom_view/agora_html.dart';
 import 'package:agora/design/custom_view/agora_like_view.dart';
 import 'package:agora/design/custom_view/agora_read_more_text.dart';
 import 'package:agora/design/custom_view/agora_scaffold.dart';
