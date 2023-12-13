68534a48775968875df4b5c4f8cb61eede0f1bfa
Thierry LEE <thierry.leet@gmail.com>
2023-12-13 18:04:26 +0100

    Perform SSL pinning on prod flavor only

--- lib/common/client/agora_http_client_adapter.dart
+++ lib/common/client/agora_http_client_adapter.dart
@@ -1,5 +1,6 @@
 import 'dart:typed_data';
 
+import 'package:agora/common/helper/flavor_helper.dart';
 import 'package:basic_utils/basic_utils.dart';
 import 'package:dio_http2_adapter/dio_http2_adapter.dart';
 
@@ -16,16 +17,21 @@ class AgoraHttpClientAdapter extends Http2Adapter {
     required String baseUrlHost,
     required X509CertificateData rootCertificateX509,
   }) {
-    return ConnectionManager(
-      onClientCreate: (_, config) {
-        config.validateCertificate = (certificate, host, _) {
-          if (host == baseUrlHost && certificate != null) {
-            final serverX509 = X509Utils.x509CertificateFromPem(X509Utils.crlDerToPem(certificate.der));
-            return X509Utils.checkChain([serverX509, rootCertificateX509]).isValid();
-          }
-          return true;
-        };
-      },
-    );
+    switch (FlavorHelper.getFlavor()) {
+      case AgoraFlavor.dev:
+        return ConnectionManager();
+      case AgoraFlavor.prod:
+        return ConnectionManager(
+          onClientCreate: (_, config) {
+            config.validateCertificate = (certificate, host, _) {
+              if (host == baseUrlHost && certificate != null) {
+                final serverX509 = X509Utils.x509CertificateFromPem(X509Utils.crlDerToPem(certificate.der));
+                return X509Utils.checkChain([serverX509, rootCertificateX509]).isValid();
+              }
+              return true;
+            };
+          },
+        );
+    }
   }
 }
