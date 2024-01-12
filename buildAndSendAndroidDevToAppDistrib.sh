4bcdc932168bd0fee24777e96c5714896b10dfbe
remi.dormoy <remi.dormoy@accenture.com>
2024-01-12 09:38:16 +0100

    ajout fichier sh pour builder sur app distrib

---
+++ buildAndSendAndroidDevToAppDistrib.sh
@@ -0,0 +1,6 @@
+flutter build appbundle --flavor dev -t lib/main_dev.dart --dart-define=app.flavor=Dev
+rm build/app/outputs/bundle/devRelease/app-dev-release.apks
+bundletool build-apks --bundle=build/app/outputs/bundle/devRelease/app-dev-release.aab --output=build/app/outputs/bundle/devRelease/app-dev-release.apks --mode=universal
+tar -xzvf build/app/outputs/bundle/devRelease/app-dev-release.apks -C build/app/outputs/bundle/devRelease/
+mv build/app/outputs/bundle/devRelease/universal.apk build/app/outputs/bundle/devRelease/app-dev-release.apk
+firebase appdistribution:distribute build/app/outputs/bundle/devRelease/app-dev-release.apk --app 1:128862173768:android:e96bec772348b56fb3f67d --release-notes "Nom à changer"
