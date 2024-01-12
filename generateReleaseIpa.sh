21bf3ed1bdfc1f15e1e557ab44da690375c070b3
Thierry LEE <thierry.leet@gmail.com>
2024-01-12 12:24:26 +0100

    Version updated to 1.3.1

--- generateReleaseIpa.sh
+++ generateReleaseIpa.sh
@@ -1,1 +1,1 @@
-flutter build ipa --target=lib/main_prod.dart --flavor=prod --dart-define=app.flavor=Prod --export-options-plist=ios/Params/ExportOptionsProd.plist --build-name="1.3.0" --build-number=31
+flutter build ipa --target=lib/main_prod.dart --flavor=prod --dart-define=app.flavor=Prod --export-options-plist=ios/Params/ExportOptionsProd.plist --build-name="1.3.1" --build-number=32
