215da84da189ed814fa9f86413b96032fd751470
Thierry LEE <thierry.leet@gmail.com>
2024-01-23 16:03:12 +0100

    Try to fix loop error on boot

--- lib/common/storage/secure_storage_client.dart
+++ lib/common/storage/secure_storage_client.dart
@@ -18,23 +18,39 @@ class FlutterSecureStorageClient extends SecureStorageClient {
   FlutterSecureStorageClient({required this.secureStorage, required this.sharedPref}) {
     final isFirstRun = sharedPref.getBool(firstRunKey) ?? true;
     if (isFirstRun) {
-      secureStorage.deleteAll();
+      try {
+        secureStorage.deleteAll();
+      } catch (e) {
+        // Do nothing
+      }
       sharedPref.setBool(firstRunKey, false);
     }
   }
 
   @override
   Future<void> write({required String key, required String value}) async {
-    await secureStorage.write(key: key, value: value);
+    try {
+      await secureStorage.write(key: key, value: value);
+    } catch (e) {
+      return;
+    }
   }
 
   @override
   Future<String?> read({required String key}) async {
-    return await secureStorage.read(key: key);
+    try {
+      return await secureStorage.read(key: key);
+    } catch (e) {
+      return null;
+    }
   }
 
   @override
   Future<void> delete({required String key}) async {
-    return await secureStorage.delete(key: key);
+    try {
+      return await secureStorage.delete(key: key);
+    } catch (e) {
+      return;
+    }
   }
 }
