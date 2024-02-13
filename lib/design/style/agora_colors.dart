3f9dd081a1d87bf50cdd65661e6315f7bc5b911d
Thierry LEE <thierry.leet@gmail.com>
2024-02-13 15:18:28 +0100

    Improve video player accessibility

--- lib/design/style/agora_colors.dart
+++ lib/design/style/agora_colors.dart
@@ -51,3 +51,20 @@ class AgoraColors {
   static const Color hintColor = Color(0xFF767676);
   static const Color borderHintColor = Color(0xFF8D8D8D);
 }
+
+class AgoraVideoProgressColors {
+  AgoraVideoProgressColors({
+    Color playedColor = AgoraColors.primaryBlue,
+    Color bufferedColor = AgoraColors.blue525,
+    Color handleColor = const Color.fromRGBO(200, 200, 200, 1.0),
+    Color backgroundColor = const Color.fromRGBO(200, 200, 200, 0.5),
+  })  : playedPaint = Paint()..color = playedColor,
+        bufferedPaint = Paint()..color = bufferedColor,
+        handlePaint = Paint()..color = handleColor,
+        backgroundPaint = Paint()..color = backgroundColor;
+
+  final Paint playedPaint;
+  final Paint bufferedPaint;
+  final Paint handlePaint;
+  final Paint backgroundPaint;
+}
