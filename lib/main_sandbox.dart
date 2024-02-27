979e3625a2862cae0e9eee132f2d541ef1e48703
Thierry LEE <thierry.leet@gmail.com>
2024-02-27 18:06:39 +0100

    Update Sandbox API URL

--- lib/main_sandbox.dart
+++ lib/main_sandbox.dart
@@ -5,7 +5,7 @@ import 'package:agora/agora_app_initializer.dart';
 void main() {
   AgoraInitializer.initializeApp(
     AgoraAppConfig(
-      baseUrl: "https://agora-back-sandbox.osc-fr1.scalingo.io",
+      baseUrl: "https://api.sandbox.agora.incubateur.net",
       environmentName: 'sandbox',
       sentryDsn: 'https://f3964b7a38e0470a8ab7a23758ceb911@sentry.incubateur.net/122',
       appIcon: "assets/launcher_icons/ic_agora_sandbox_logo.png",
