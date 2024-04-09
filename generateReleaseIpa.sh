ce5eedd3ebf1b477e736e4d3d188c9bb440dfb8c
Thierry LEE <thierry.leet@gmail.com>
2024-04-09 16:41:23 +0200

    Version code update

--- generateReleaseIpa.sh
+++ generateReleaseIpa.sh
@@ -1,1 +1,1 @@
-flutter build ipa --target=lib/main_prod.dart --flavor=prod --dart-define=app.flavor=Prod --export-options-plist=ios/Params/ExportOptionsProd.plist --build-name="2.0.0" --build-number=46
+flutter build ipa --target=lib/main_prod.dart --flavor=prod --dart-define=app.flavor=Prod --export-options-plist=ios/Params/ExportOptionsProd.plist --build-name="2.0.0" --build-number=47
