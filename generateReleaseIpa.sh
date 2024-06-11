6a9a4883c815ffc1b7768fe1644797e1a0c7d6af
ENZO <79970665+encalv@users.noreply.github.com>
2024-06-11 09:43:21 +0200

    Release 3.0.0 : Mise à jour version (#505)

--- generateReleaseIpa.sh
+++ generateReleaseIpa.sh
@@ -1,1 +1,1 @@
-flutter build ipa --target=lib/main_prod.dart --flavor=prod --dart-define=app.flavor=Prod --export-options-plist=ios/Params/ExportOptionsProd.plist --build-name="2.0.3" --build-number=50
+flutter build ipa --target=lib/main_prod.dart --flavor=prod --dart-define=app.flavor=Prod --export-options-plist=ios/Params/ExportOptionsProd.plist --build-name="3.0.0" --build-number=50
