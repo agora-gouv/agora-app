ab4ca810f4b8eca5cb2e4b2712564caeda66c684
Enzo Calvino <enzo.calvino@octo.com>
2024-06-14 11:27:57 +0200

    update pub firebase core

--- lib/common/analytics/matomo_config.dart
+++ lib/common/analytics/matomo_config.dart
@@ -1,9 +1,9 @@
 class MatomoConfig {
-  final int siteId;
+  final String siteId;
   final String url;
 
   MatomoConfig({
-    this.siteId = 21,
+    this.siteId = "21",
     this.url = "https://stats.beta.gouv.fr/matomo.php",
   });
 }
