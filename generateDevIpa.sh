41203f6a251dedc84f2fd3fbaaf3d1e468628b34
ENZO <79970665+encalv@users.noreply.github.com>
2024-08-21 15:20:43 +0200

    [TECH] Mise à jour vers la 3.1.0 (#559)

--- generateDevIpa.sh
+++ generateDevIpa.sh
@@ -1,1 +1,1 @@
-flutter build ipa --target=lib/main_dev.dart --flavor=dev --dart-define=app.flavor=Dev --export-options-plist=ios/Params/ExportOptionsAdHoc.plist --build-name="3.0.1" --build-number=60
+flutter build ipa --target=lib/main_dev.dart --flavor=dev --dart-define=app.flavor=Dev --export-options-plist=ios/Params/ExportOptionsAdHoc.plist --build-name="3.1.0" --build-number=60
