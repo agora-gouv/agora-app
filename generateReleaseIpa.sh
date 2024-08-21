e072c528f1c2da3ca8b8dcaf0bb828943d009b05
enzo.calvino <enzo.calvino@octo.com>
2024-08-21 15:13:57 +0200

    update current version to 3.1.0

--- generateReleaseIpa.sh
+++ generateReleaseIpa.sh
@@ -1,1 +1,1 @@
-flutter build ipa --target=lib/main_prod.dart --flavor=prod --dart-define=app.flavor=Prod --export-options-plist=ios/Params/ExportOptionsProd.plist --build-name="3.0.1" --build-number=60
+flutter build ipa --target=lib/main_prod.dart --flavor=prod --dart-define=app.flavor=Prod --export-options-plist=ios/Params/ExportOptionsProd.plist --build-name="3.1.0" --build-number=60
