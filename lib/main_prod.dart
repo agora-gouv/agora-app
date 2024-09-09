4356d89fe2e5b68d94de88653435e085a2a57a35
enzo.calvino <enzo.calvino@octo.com>
2024-09-09 10:18:23 +0200

    changement liens

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -4,7 +4,7 @@ import 'package:agora/design/style/agora_colors.dart';
 void main() {
   AgoraInitializer.initializeApp(
     AgoraAppConfig(
-      baseUrl: "https://api.agora.beta.gouv.fr",
+      baseUrl: "https://api.agora.gouv.fr",
       environmentName: 'prod',
       sentryDsn: 'https://f3964b7a38e0470a8ab7a23758ceb911@sentry.incubateur.net/122',
       appIcon: "assets/launcher_icons/ic_agora_logo.png",
