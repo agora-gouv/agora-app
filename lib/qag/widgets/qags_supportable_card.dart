f91b744d5854ef9018976be5ff0fd3ea4f477e15
ENZO <79970665+encalv@users.noreply.github.com>
2024-09-10 14:36:17 +0200

    [FIX] Bug sur les likes QaG (#581)

--- lib/qag/widgets/qags_supportable_card.dart
+++ lib/qag/widgets/qags_supportable_card.dart
@@ -1,5 +1,6 @@
 import 'package:agora/common/analytics/analytics_event_names.dart';
 import 'package:agora/common/helper/tracker_helper.dart';
+import 'package:agora/common/manager/repository_manager.dart';
 import 'package:agora/design/custom_view/agora_like_animation_view.dart';
 import 'package:agora/design/custom_view/agora_like_view.dart';
 import 'package:agora/design/custom_view/card/agora_question_card.dart';
@@ -30,77 +31,97 @@ class QagsSupportableCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final likeAnimationView = AgoraLikeAnimationView(animationControllerKey: GlobalKey(), likeViewKey: likeViewKey);
-    return Stack(
-      children: [
-        BlocSelector<QagSupportBloc, QagSupportState, _ViewModel>(
-          selector: (supportState) => _toViewModel(supportState),
-          builder: (context, viewModel) {
-            return AgoraQuestionCard(
-              id: qagViewModel.id,
-              thematique: qagViewModel.thematique,
-              titre: qagViewModel.title,
-              nom: qagViewModel.username,
-              date: qagViewModel.date,
-              supportCount: viewModel.supportCount(),
-              isSupported: viewModel.isSupported(),
-              isAuthor: qagViewModel.isAuthor,
-              likeViewKey: likeViewKey,
-              onSupportClick: (bool support) {
-                if (support) {
-                  TrackerHelper.trackClick(
-                    clickName: AnalyticsEventNames.likeQag,
-                    widgetName: widgetName,
-                  );
-                  context.read<QagSupportBloc>().add(SupportQagEvent(qagId: qagViewModel.id));
-                } else {
-                  TrackerHelper.trackClick(
-                    clickName: AnalyticsEventNames.unlikeQag,
-                    widgetName: widgetName,
-                  );
-                  context.read<QagSupportBloc>().add(DeleteSupportQagEvent(qagId: qagViewModel.id));
-                }
-              },
-              onCardClick: () {
-                Navigator.pushNamed(
-                  context,
-                  QagDetailsPage.routeName,
-                  arguments: QagDetailsArguments(qagId: qagViewModel.id, reload: QagReload.qagsPage), // FIXME
-                ).then((result) {
-                  if (onQagSupportChange != null && result != null && result is QagDetailsBackResult) {
-                    onQagSupportChange!(
-                      QagSupport(
-                        qagId: result.qagId,
-                        isSupported: result.isSupported,
-                        supportCount: result.supportCount,
-                      ),
+    return BlocProvider(
+      create: (context) => QagSupportBloc(qagRepository: RepositoryManager.getQagRepository()),
+      child: Stack(
+        children: [
+          BlocSelector<QagSupportBloc, QagSupportState, _ViewModel>(
+            selector: (supportState) => _toViewModel(supportState),
+            builder: (context, viewModel) {
+              return AgoraQuestionCard(
+                id: qagViewModel.id,
+                thematique: qagViewModel.thematique,
+                titre: qagViewModel.title,
+                nom: qagViewModel.username,
+                date: qagViewModel.date,
+                supportCount: viewModel.supportCount,
+                isSupported: viewModel.isSupported,
+                isAuthor: qagViewModel.isAuthor,
+                likeViewKey: likeViewKey,
+                onSupportClick: (bool support) {
+                  if (support) {
+                    TrackerHelper.trackClick(
+                      clickName: AnalyticsEventNames.likeQag,
+                      widgetName: widgetName,
                     );
+                    context.read<QagSupportBloc>().add(
+                          SupportQagEvent(
+                            qagId: qagViewModel.id,
+                            supportCount: viewModel.supportCount,
+                            isSupported: viewModel.isSupported,
+                          ),
+                        );
+                  } else {
+                    TrackerHelper.trackClick(
+                      clickName: AnalyticsEventNames.unlikeQag,
+                      widgetName: widgetName,
+                    );
+                    context.read<QagSupportBloc>().add(
+                          DeleteSupportQagEvent(
+                            qagId: qagViewModel.id,
+                            supportCount: viewModel.supportCount,
+                            isSupported: viewModel.isSupported,
+                          ),
+                        );
                   }
-                });
-              },
-            );
-          },
-        ),
-        BlocListener<QagSupportBloc, QagSupportState>(
-          listenWhen: (previousState, currentState) {
-            if (!_toLikeViewModel(previousState).isSupported && _toLikeViewModel(currentState).isSupported) {
-              likeAnimationView.animate();
-            }
-            if (_toLikeViewModel(previousState).isSupported && !_toLikeViewModel(currentState).isSupported) {
-              likeAnimationView.reverseAnimate();
-            }
-            return false;
-          },
-          listener: (context, state) => {},
-          child: likeAnimationView,
-        ),
-      ],
+                },
+                onCardClick: () {
+                  Navigator.pushNamed(
+                    context,
+                    QagDetailsPage.routeName,
+                    arguments: QagDetailsArguments(
+                      qagId: qagViewModel.id,
+                      reload: QagReload.qagsPage,
+                      qagSupportBloc: context.read<QagSupportBloc>(),
+                    ),
+                  ).then((result) {
+                    if (onQagSupportChange != null && result != null && result is QagDetailsBackResult) {
+                      onQagSupportChange!(
+                        QagSupport(
+                          qagId: result.qagId,
+                          isSupported: result.isSupported,
+                          supportCount: result.supportCount,
+                        ),
+                      );
+                    }
+                  });
+                },
+              );
+            },
+          ),
+          BlocListener<QagSupportBloc, QagSupportState>(
+            listenWhen: (previousState, currentState) {
+              if (!_toLikeViewModel(previousState).isSupported && _toLikeViewModel(currentState).isSupported) {
+                likeAnimationView.animate();
+              }
+              if (_toLikeViewModel(previousState).isSupported && !_toLikeViewModel(currentState).isSupported) {
+                likeAnimationView.reverseAnimate();
+              }
+              return false;
+            },
+            listener: (context, state) => {},
+            child: likeAnimationView,
+          ),
+        ],
+      ),
     );
   }
 
   _ViewModel _toViewModel(QagSupportState supportState) {
     return _ViewModel(
-      isLoading: supportState is QagSupportLoadingState || supportState is QagDeleteSupportLoadingState,
-      viewModel: _toLikeViewModel(supportState),
+      isLoading: supportState is QagSupportLoadingState,
+      isSupported: _buildIsSupported(supportState),
+      supportCount: _buildCount(supportState),
     );
   }
 
@@ -111,7 +132,7 @@ class QagsSupportableCard extends StatelessWidget {
     );
   }
 
-  bool _isInitiallySupported() {
+  bool _initialIsSupported() {
     return qagViewModel.isSupported;
   }
 
@@ -119,19 +140,15 @@ class QagsSupportableCard extends StatelessWidget {
     return qagViewModel.supportCount;
   }
 
-  int _supportCountWhenUnliked() {
-    return _initialSupportCount() - (_isInitiallySupported() ? 1 : 0);
-  }
-
   bool _buildIsSupported(QagSupportState supportState) {
     if (supportState is QagSupportInitialState) {
-      return _isInitiallySupported();
+      return _initialIsSupported();
     } else if (supportState is QagSupportLoadingState) {
-      return !_isInitiallySupported();
+      return !_initialIsSupported();
     } else {
-      if (supportState is QagSupportSuccessState || supportState is QagDeleteSupportErrorState) {
-        return true;
-      } else if (supportState is QagSupportErrorState || supportState is QagDeleteSupportSuccessState) {
+      if (supportState is QagSupportSuccessState) {
+        return supportState.isSupported;
+      } else if (supportState is QagSupportErrorState) {
         return false;
       }
     }
@@ -141,10 +158,8 @@ class QagsSupportableCard extends StatelessWidget {
   int _buildCount(QagSupportState supportState) {
     if (supportState is QagSupportInitialState) {
       return _initialSupportCount();
-    } else if (supportState is QagSupportSuccessState || supportState is QagDeleteSupportErrorState) {
-      return _supportCountWhenUnliked() + 1;
-    } else if (supportState is QagDeleteSupportSuccessState || supportState is QagDeleteSupportSuccessState) {
-      return _supportCountWhenUnliked();
+    } else if (supportState is QagSupportSuccessState) {
+      return supportState.supportCount;
     }
     return _initialSupportCount();
   }
@@ -152,18 +167,11 @@ class QagsSupportableCard extends StatelessWidget {
 
 class _ViewModel extends Equatable {
   final bool isLoading;
-  final AgoraLikeViewModel viewModel;
+  final bool isSupported;
+  final int supportCount;
 
-  _ViewModel({required this.isLoading, required this.viewModel});
-
-  bool isSupported() {
-    return viewModel.isSupported;
-  }
-
-  int supportCount() {
-    return viewModel.supportCount;
-  }
+  _ViewModel({required this.isLoading, required this.isSupported, required this.supportCount});
 
   @override
-  List<Object?> get props => [viewModel];
+  List<Object?> get props => [isLoading, isSupported, supportCount];
 }
