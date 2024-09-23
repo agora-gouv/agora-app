7e6c9bf9959c4203c18416c64f03ad7e7dd905b9
ATMA <pheno62@gmail.com>
2024-09-23 11:34:35 +0200

    feat : multi certificats

--- lib/agora_app_initializer.dart
+++ lib/agora_app_initializer.dart
@@ -7,6 +7,7 @@ import 'package:agora/common/manager/service_manager.dart';
 import 'package:agora/common/manager/storage_manager.dart';
 import 'package:agora/common/observer/event_observer.dart';
 import 'package:agora/consultation/question/bloc/response/stock/consultation_question_response_hive.dart';
+import 'package:basic_utils/basic_utils.dart';
 import 'package:equatable/equatable.dart';
 import 'package:error_stack/error_stack.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -60,8 +61,24 @@ class AgoraInitializer {
     );
   }
 
-  static Future<Uint8List> _readCertificate() async {
-    return (await rootBundle.load("assets/certificates/certificate.cer")).buffer.asUint8List();
+  static Future<List<X509CertificateData>> _readCertificate() async {
+    // TODO pour tester on peut pinguer des sites qui utilisent Certigna ou LetsEncrypt
+    final certignaCertificate =
+        X509Utils.crlDerToPem((await rootBundle.load("assets/certificates/certigna-rootca.cer")).buffer.asUint8List());
+    final letsEncryptCertificates = [
+      (await rootBundle.loadString("assets/certificates/letsencrypt-isrg-root-x1.pem")),
+      (await rootBundle.loadString("assets/certificates/letsencrypt-isrg-root-x2.pem")),
+      (await rootBundle.loadString("assets/certificates/letsencrypt-e5.pem")),
+      (await rootBundle.loadString("assets/certificates/letsencrypt-e6.pem")),
+      (await rootBundle.loadString("assets/certificates/letsencrypt-r10.pem")),
+      (await rootBundle.loadString("assets/certificates/letsencrypt-r11.pem")),
+    ];
+
+    final rawCertificates = [certignaCertificate, ...letsEncryptCertificates];
+    final certificates =
+        rawCertificates.map((rawCertificate) => X509Utils.x509CertificateFromPem(rawCertificate)).toList();
+
+    return certificates;
   }
 
   static Future<void> _setupNotification() async {
