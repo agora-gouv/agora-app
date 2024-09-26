82bad59852150eec2f7d279da689e9462f80a55a
enzo.calvino <enzo.calvino@octo.com>
2024-09-26 15:23:22 +0200

    version 3.2.0

--- generateDevIpa.sh
+++ generateDevIpa.sh
@@ -1,1 +1,1 @@
-flutter build ipa --target=lib/main_dev.dart --flavor=dev --dart-define=app.flavor=Dev --export-options-plist=ios/Params/ExportOptionsAdHoc.plist --build-name="3.2.0" --build-number=70
+flutter build ipa --target=lib/main_dev.dart --flavor=dev --dart-define=app.flavor=Dev --export-options-plist=ios/Params/ExportOptionsAdHoc.plist --build-name="3.2.1" --build-number=80
