a6787c354e75d4247d6d98f4077ec8107450f463
enzo.calvino <enzo.calvino@octo.com>
2024-10-02 12:07:06 +0200

    feat consultations terminées paginées

--- lib/consultation/repository/consultation_finished_paginated_presenter.dart
+++ lib/consultation/repository/consultation_finished_paginated_presenter.dart
@@ -1,13 +1,14 @@
 import 'package:agora/common/extension/thematique_extension.dart';
 import 'package:agora/consultation/domain/consultation.dart';
 import 'package:agora/consultation/finished_paginated/bloc/consultation_finished_paginated_view_model.dart';
-import 'package:agora/territorialisation/territoire.dart';
+import 'package:agora/territorialisation/region.dart';
 import 'package:agora/territorialisation/territoire_helper.dart';
 
 class ConsultationFinishedPaginatedPresenter {
   static List<ConsultationPaginatedViewModel> presentPaginatedConsultations(
     List<Consultation> finishedConsultations,
     List<Consultation> concertations,
+    List<Region> referentiel,
   ) {
     final allConsultationPaginated = [...finishedConsultations, ...concertations];
     allConsultationPaginated.sort((a, b) {
@@ -16,6 +17,7 @@ class ConsultationFinishedPaginatedPresenter {
     });
     return allConsultationPaginated.map(
       (consultation) {
+        final territoire = getTerritoireFromReferentiel(referentiel, consultation.territoire);
         return ConsultationPaginatedViewModel(
           id: consultation.id,
           title: consultation.title,
@@ -23,9 +25,9 @@ class ConsultationFinishedPaginatedPresenter {
           thematique: consultation.thematique.toThematiqueViewModel(),
           label: consultation.label,
           externalLink: consultation is Concertation ? consultation.externalLink : null,
-          badgeLabel: "consultation.territoire.label.toUpperCase()",
-          badgeColor: getTerritoireBadgeColor(TerritoireType.departemental),
-          badgeTextColor: getTerritoireBadgeTexteColor(TerritoireType.departemental),
+          badgeLabel: territoire.label.toUpperCase(),
+          badgeColor: getTerritoireBadgeColor(territoire.type),
+          badgeTextColor: getTerritoireBadgeTexteColor(territoire.type),
         );
       },
     ).toList();
