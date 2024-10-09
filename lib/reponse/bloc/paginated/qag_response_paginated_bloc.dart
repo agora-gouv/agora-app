2119d917255c6a8b8b31b93a0588f9274a95a377
enzo.calvino <enzo.calvino@octo.com>
2024-10-09 18:19:43 +0200

    fix

--- lib/reponse/bloc/paginated/qag_response_paginated_bloc.dart
+++ lib/reponse/bloc/paginated/qag_response_paginated_bloc.dart
@@ -1,7 +1,8 @@
-import 'package:agora/reponse/bloc/paginated/qag_response_paginated_event.dart';
-import 'package:agora/reponse/bloc/paginated/qag_response_paginated_state.dart';
+import 'package:agora/common/helper/all_purpose_status.dart';
 import 'package:agora/qag/repository/presenter/qag_response_paginated_presenter.dart';
 import 'package:agora/qag/repository/qag_repository.dart';
+import 'package:agora/reponse/bloc/paginated/qag_response_paginated_event.dart';
+import 'package:agora/reponse/bloc/paginated/qag_response_paginated_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class QagResponsePaginatedBloc extends Bloc<FetchQagsResponsePaginatedEvent, QagResponsePaginatedState> {
@@ -9,7 +10,7 @@ class QagResponsePaginatedBloc extends Bloc<FetchQagsResponsePaginatedEvent, Qag
 
   QagResponsePaginatedBloc({
     required this.qagRepository,
-  }) : super(QagResponsePaginatedInitialState()) {
+  }) : super(QagResponsePaginatedState.init()) {
     on<FetchQagsResponsePaginatedEvent>(_handleFetchQagResponsePaginated);
   }
 
@@ -18,7 +19,8 @@ class QagResponsePaginatedBloc extends Bloc<FetchQagsResponsePaginatedEvent, Qag
     Emitter<QagResponsePaginatedState> emit,
   ) async {
     emit(
-      QagResponsePaginatedLoadingState(
+      state.clone(
+        status: AllPurposeStatus.loading,
         maxPage: event.pageNumber == 1 ? -1 : state.maxPage,
         currentPageNumber: event.pageNumber,
         qagResponseViewModels: event.pageNumber == 1 ? [] : state.qagResponseViewModels,
@@ -30,7 +32,8 @@ class QagResponsePaginatedBloc extends Bloc<FetchQagsResponsePaginatedEvent, Qag
         paginatedQagsResponse: response.paginatedQagsResponse,
       );
       emit(
-        QagResponsePaginatedFetchedState(
+        state.clone(
+          status: AllPurposeStatus.success,
           maxPage: response.maxPage,
           currentPageNumber: event.pageNumber,
           qagResponseViewModels: event.pageNumber == 1 ? viewModels : state.qagResponseViewModels + viewModels,
@@ -38,7 +41,8 @@ class QagResponsePaginatedBloc extends Bloc<FetchQagsResponsePaginatedEvent, Qag
       );
     } else {
       emit(
-        QagResponsePaginatedErrorState(
+        state.clone(
+          status: AllPurposeStatus.error,
           maxPage: state.maxPage,
           currentPageNumber: event.pageNumber,
           qagResponseViewModels: state.qagResponseViewModels,
