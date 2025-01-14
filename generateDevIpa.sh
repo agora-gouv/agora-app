4f30355d4589a667d8aa62fd031f3bc584530e8d
Antoine Mazure <pheno62@gmail.com>
2025-01-14 16:42:28 +0100

    [TECH] Mise à jour version 3.6.0 (#645)

--- generateDevIpa.sh
+++ generateDevIpa.sh
@@ -1,1 +1,1 @@
-flutter build ipa --target=lib/main_dev.dart --flavor=dev --dart-define=app.flavor=Dev --export-options-plist=ios/Params/ExportOptionsAdHoc.plist --build-name="3.5.0" --build-number=112
+flutter build ipa --target=lib/main_dev.dart --flavor=dev --dart-define=app.flavor=Dev --export-options-plist=ios/Params/ExportOptionsAdHoc.plist --build-name="3.6.0" --build-number=120
