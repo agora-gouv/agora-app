64fbeff65dc523d0255c28e255107f4b0a75974d
ATMA <pheno62@gmail.com>
2025-01-14 16:39:19 +0100

    [TECH] Mise à jour version 3.6.0

--- generateReleaseIpa.sh
+++ generateReleaseIpa.sh
@@ -1,1 +1,1 @@
-flutter build ipa --target=lib/main_prod.dart --flavor=prod --dart-define=app.flavor=Prod --export-options-plist=ios/Params/ExportOptionsProd.plist --build-name="3.5.0" --build-number=112
+flutter build ipa --target=lib/main_prod.dart --flavor=prod --dart-define=app.flavor=Prod --export-options-plist=ios/Params/ExportOptionsProd.plist --build-name="3.6.0" --build-number=120
