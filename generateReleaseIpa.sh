3c7093e49b76abbd208cb274118a884973b89a78
ATMA <pheno62@gmail.com>
2025-01-31 16:24:21 +0100

    Release 3.7.1

--- generateReleaseIpa.sh
+++ generateReleaseIpa.sh
@@ -1,1 +1,1 @@
-flutter build ipa --target=lib/main_prod.dart --flavor=prod --dart-define=app.flavor=Prod --export-options-plist=ios/Params/ExportOptionsProd.plist --build-name="3.7.0" --build-number=125
+flutter build ipa --target=lib/main_prod.dart --flavor=prod --dart-define=app.flavor=Prod --export-options-plist=ios/Params/ExportOptionsProd.plist --build-name="3.7.1" --build-number=126
