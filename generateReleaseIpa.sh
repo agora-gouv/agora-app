2aac03019f0abf7dfd84c153593f1cd111a28b06
Antoine Mazure <pheno62@gmail.com>
2025-02-12 11:11:01 +0100

    [TECH] Mise à jour version 3.7.2 (#660)

--- generateReleaseIpa.sh
+++ generateReleaseIpa.sh
@@ -1,1 +1,1 @@
-flutter build ipa --target=lib/main_prod.dart --flavor=prod --dart-define=app.flavor=Prod --export-options-plist=ios/Params/ExportOptionsProd.plist --build-name="3.7.1" --build-number=126
+flutter build ipa --target=lib/main_prod.dart --flavor=prod --dart-define=app.flavor=Prod --export-options-plist=ios/Params/ExportOptionsProd.plist --build-name="3.7.2" --build-number=127
