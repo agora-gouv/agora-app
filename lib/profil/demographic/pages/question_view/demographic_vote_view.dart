7393dcba5de60e8e02c2875eaacb43337fee5af6
Antoine Mazure <pheno62@gmail.com>
2025-02-12 11:03:36 +0100

    feat : ajout de bottomsheet (#659)

--- lib/profil/demographic/pages/question_view/demographic_vote_view.dart
+++ lib/profil/demographic/pages/question_view/demographic_vote_view.dart
@@ -1,9 +1,8 @@
 import 'package:agora/common/strings/demographic_strings.dart';
-import 'package:agora/common/strings/generic_strings.dart';
-import 'package:agora/design/custom_view/agora_alert_dialog.dart';
 import 'package:agora/design/custom_view/agora_demographic_simple_view.dart';
-import 'package:agora/design/custom_view/button/agora_button.dart';
+import 'package:agora/design/custom_view/bottom_sheet/agora_bottom_sheet.dart';
 import 'package:agora/design/custom_view/text/agora_link_text.dart';
+import 'package:agora/design/style/agora_colors.dart';
 import 'package:agora/design/style/agora_spacings.dart';
 import 'package:agora/design/style/agora_text_styles.dart';
 import 'package:agora/profil/demographic/domain/demographic_question_type.dart';
@@ -72,17 +71,18 @@ class _DemographicVoteViewState extends State<DemographicVoteView> {
           label: DemographicStrings.whyAsk,
           textPadding: EdgeInsets.zero,
           onTap: () {
-            showAgoraDialog(
+            showModalBottomSheet(
               context: context,
-              columnChildren: [
-                Text(DemographicStrings.whyAskContent, style: AgoraTextStyles.light16),
-                SizedBox(height: AgoraSpacings.x0_75),
-                AgoraButton.withLabel(
-                  label: GenericStrings.close,
-                  buttonStyle: AgoraButtonStyle.primary,
-                  onPressed: () => Navigator.pop(context),
+              isScrollControlled: true,
+              backgroundColor: AgoraColors.transparent,
+              builder: (context) => AgoraInformationBottomSheet(
+                titre: DemographicStrings.whyAsk,
+                description: Text(
+                  DemographicStrings.whyAskContent,
+                  textAlign: TextAlign.center,
+                  style: AgoraTextStyles.light16,
                 ),
-              ],
+              ),
             );
           },
         ),
