4ff430d3ee08e8af3c8e1ffc1059fe4b76279cd4
ATMA <pheno62@gmail.com>
2025-03-13 11:02:08 +0100

    tech : update 3.7.5

--- generateDevIpa.sh
+++ generateDevIpa.sh
@@ -1,1 +1,1 @@
-flutter build ipa --target=lib/main_dev.dart --flavor=dev --dart-define=app.flavor=Dev --export-options-plist=ios/Params/ExportOptionsAdHoc.plist --build-name="3.7.4" --build-number=130
+flutter build ipa --target=lib/main_dev.dart --flavor=dev --dart-define=app.flavor=Dev --export-options-plist=ios/Params/ExportOptionsAdHoc.plist --build-name="3.7.5" --build-number=131
