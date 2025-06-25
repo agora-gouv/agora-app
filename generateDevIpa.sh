0b202d0133a7566a9bb28fa391b486f0c661ea56
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2025-06-25 12:18:22 +0200

    wip: Update dev IPA with extracted version and build numbers

--- generateDevIpa.sh
+++ generateDevIpa.sh
@@ -1,1 +1,8 @@
-flutter build ipa --target=lib/main_dev.dart --flavor=dev --dart-define=app.flavor=Dev --export-options-plist=ios/Params/ExportOptionsAdHoc.plist --build-name="3.7.5" --build-number=131
+#!/bin/sh
+
+source ./scripts/extractVersion.sh
+PUBSPEC="./pubspec.yaml"
+VERSION=$(getVersion $PUBSPEC)
+BUILD_NUMBER=$(getBuildNumber $PUBSPEC)
+
+flutter build ipa --target=lib/main_dev.dart --flavor=dev --dart-define=app.flavor=Dev --export-options-plist=ios/Params/ExportOptionsAdHoc.plist --build-name="${VERSION}" --build-number=${BUILD_NUMBER}
