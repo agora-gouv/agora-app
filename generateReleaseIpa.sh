b857e0deb1ae7236e9735aa1670151c80f1b34b1
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2025-06-25 12:21:06 +0200

    wip: Update sandbox and release IPA with build and version numbers

--- generateReleaseIpa.sh
+++ generateReleaseIpa.sh
@@ -1,1 +1,8 @@
-flutter build ipa --target=lib/main_prod.dart --flavor=prod --dart-define=app.flavor=Prod --export-options-plist=ios/Params/ExportOptionsProd.plist --build-name="3.7.5" --build-number=131
+#!/bin/sh
+
+source ./scripts/extractVersion.sh
+PUBSPEC="./pubspec.yaml"
+VERSION=$(getVersion $PUBSPEC)
+BUILD_NUMBER=$(getBuildNumber $PUBSPEC)
+
+flutter build ipa --target=lib/main_prod.dart --flavor=prod --dart-define=app.flavor=Prod --export-options-plist=ios/Params/ExportOptionsProd.plist --build-name="${VERSION}" --build-number=${BUILD_NUMBER}
--- generateSandboxIpa.sh
+++ generateSandboxIpa.sh
@@ -1,1 +1,8 @@
-flutter build ipa --target=lib/main_sandbox.dart --flavor=sandbox --dart-define=app.flavor=Sandbox --export-options-plist=ios/Params/ExportOptionsAdHoc.plist --build-name="1.3.2" --build-number=36
+#!/bin/sh
+
+source ./scripts/extractVersion.sh
+PUBSPEC="./pubspec.yaml"
+VERSION=$(getVersion $PUBSPEC)
+BUILD_NUMBER=$(getBuildNumber $PUBSPEC)
+
+flutter build ipa --target=lib/main_sandbox.dart --flavor=sandbox --dart-define=app.flavor=Sandbox --export-options-plist=ios/Params/ExportOptionsAdHoc.plist --build-name="${VERSION}" --build-number=${BUILD_NUMBER}
