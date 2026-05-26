b92a210aef65e75068a41e393c9bbec8235ef25b
alban.fontaine <alban.fontaine@octo.com>
2026-05-26 10:37:58 +0200

    feat: Implementation API tuile thème hebdo

---
+++ lib/qag/theme/bloc/qags_theme_event.dart
@@ -0,0 +1,8 @@
+import 'package:equatable/equatable.dart';
+
+abstract class QagsThemeEvent extends Equatable {}
+
+class FetchQagsThemeEvent extends QagsThemeEvent {
+  @override
+  List<Object?> get props => [];
+}
